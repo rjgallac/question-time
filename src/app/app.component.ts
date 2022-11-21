6af94f2f1be78e8111c5dcbcfb9b56ef53762d8c
rob <rjgallac@hotmail.com>
2022-11-21 21:45:41 +0000

    first form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface Survey {
+  title: string;
+  questions:any[];
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'question-time';
+
+  survey$: Observable<Survey> = new Observable();
+  answers:any[] = [];
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit() {
+    this.survey$ = this.http
+    .get<Survey>("https://q4gkevhaej.execute-api.eu-west-2.amazonaws.com/basicLambdaTest?id=789e8526-7ff9-4d5a-b3d6-2302a3d5f499");
+  }
+
+  onSubmit() {
+    console.log("HERE")
+    console.log(this.answers)
+    //this.http.post("https://q4gkevhaej.execute-api.eu-west-2.amazonaws.com/addQuestion", {});
+  }
+
 }
